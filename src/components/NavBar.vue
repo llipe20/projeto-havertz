1ebe520ae817aac9ed89a32d66609c01854ea47b
Felipe <silvahlipe@gmail.com>
2023-11-22 20:46:51 -0300

    commit

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="flex flex-col justify-center items-center w-full h-auto p-2 bg-red-500 text-white">
+    <div class="flex flex-col justify-center items-center w-full h-auto p-2 lg:gap-2 bg-red-500 text-white">
         <!-- NAV SUPERIOR -->
-        <nav class="flex justify-between items-center w-full lg:w-5/6 text-base font-light">
+        <nav class="flex justify-between items-center w-full lg:w-5/6 text-sm font-light">
             <!-- BLOCO 1 -redes -->
             <div class="hidden lg:flex justify-center items-center gap-2">
                 <a href="#" class="no-underline">Instagram</a>
@@ -42,25 +42,38 @@
             </div>
         </nav>
         <!-- NAV INTERMEDIÁRIA -->
-        <nav class="flex justify-evenly items-center w-full lg:w-5/6">
+        <nav class="flex justify-between lg:justify-evenly items-center w-full lg:w-5/6 pt-2 lg:p-2">
              <!-- BOX DE LOGO -->
-            <div class="flex justify-center items-center gap-2">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-10 h-10">
+            <div class="flex justify-center items-center gap-1 lg:gap-2">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="lg:w-10 lg:h-10 w-6 h-6">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M15.042 21.672L13.684 16.6m0 0l-2.51 2.225.569-9.47 5.227 7.917-3.286-.672zm-7.518-.267A8.25 8.25 0 1120.25 10.5M8.288 14.212A5.25 5.25 0 1117.25 10.5" />
                 </svg>
-                <span class="text-xl font-bold">
+                <span class="text-base lg:text-xl font-bold">
                     HAVERTZ
                 </span>
             </div>
             <!-- BARRA DE PESQUEISA -->
-            <div class="flex justify-center items-center gap-1">
-                <input type="text" class="h-10 w-1/2 bg-white text-black border-0 outline-0 pl-4">
-                <button class="flex justify-center items-center">
+            <div class="flex justify-between items-center gap-1 w-3/6 lg:w-3/5 bg-white pr-1">
+                <input type="text" class="w-5/6 h-10 bg-white text-black border-0 outline-0 pl-4" placeholder="Pesquisar">
+                <button class="flex justify-center items-center w-8 h-8 bg-red-500 text-white">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                       <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
                     </svg>
                 </button>
             </div>
+            <!-- BOX DE CARRINHO -->
+            <div class="flex justify-center items-center gap-1 text-white cursor-pointer">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
+                </svg>
+                <span class="hidden lg:flex">
+                    Carrinho
+                </span>
+            </div>
+        </nav>
+        <!-- NAV INFERIOR -->
+        <nav class="flex justify-between lg:justify-evenly items-center w-full lg:w-5/6 pt-2 lg:p-2">
+            
         </nav>
     </div>
 </template>
